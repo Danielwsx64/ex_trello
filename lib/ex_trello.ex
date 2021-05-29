8d1937e7d801db2025add84973e9c3069b079ba0
Danielwsx64 <danielws.x64@gmail.com>
2021-05-29 16:44:09 -0300

    Create project

---
+++ lib/ex_trello.ex
@@ -0,0 +1,18 @@
+defmodule ExTrello do
+  @moduledoc """
+  Documentation for `ExTrello`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExTrello.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_trello_test.exs
@@ -0,0 +1,8 @@
+defmodule ExTrelloTest do
+  use ExUnit.Case
+  doctest ExTrello
+
+  test "greets the world" do
+    assert ExTrello.hello() == :world
+  end
+end

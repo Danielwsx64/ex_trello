0cd8c2b63ad87b053c30c8e0c2df622d9095a022
Danielwsx64 <danielws.x64@gmail.com>
2021-05-29 17:34:40 -0300

    Configure package info

--- mix.exs
+++ mix.exs
@@ -1,11 +1,19 @@
 defmodule ExTrello.MixProject do
   use Mix.Project
 
+  @version "0.0.1"
+  @description "A client library for Trello API"
+  @links %{"GitHub" => "https://github.com/Danielwsx64/ex_trello"}
+
   def project do
     [
       app: :ex_trello,
-      version: "0.1.0",
-      elixir: "~> 1.12",
+      version: @version,
+      description: @description,
+      source_url: @links["GitHub"],
+      package: package(),
+      docs: docs(),
+      elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -17,7 +25,24 @@ defmodule ExTrello.MixProject do
     [
       # Dev/Test dependencies
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
-      {:credo_naming, "~> 1.0", only: [:dev, :test], runtime: false}
+      {:credo_naming, "~> 1.0", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.24", only: :dev, runtime: false}
+    ]
+  end
+
+  defp package do
+    [
+      licenses: ["Apache-2.0"],
+      links: @links
+    ]
+  end
+
+  defp docs do
+    [
+      source_ref: "v#{@version}",
+      main: "readme",
+      extra_section: "Guides",
+      extras: ["README.md": [title: "Get starting"]]
     ]
   end
 end
